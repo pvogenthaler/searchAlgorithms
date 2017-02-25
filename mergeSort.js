19bdcb7dc2691a57ab55f5578945a401dfa98998
Paige <paige.vogenthaler@gmail.com>
2017-02-24 22:48:30 -0800

    Adjust comments on mergeSort; Implement quickSort

--- mergeSort.js
+++ mergeSort.js
@@ -5,6 +5,8 @@ If the list is of length 0 or 1, then it is already sorted. Otherwise:
 Divide the unsorted list into two sublists of about half the size.
 Sort each sublist recursively by re-applying merge sort.
 Merge the two sublists back into one sorted list.
+
+O(n log n)
 */
 
 const mergeSort = function(arr) {
@@ -12,14 +14,14 @@ const mergeSort = function(arr) {
   const merge = function (arr1, arr2) {
     let result = [];
     while (arr1.length && arr2.length) {
-      // Add lesser value into result
+      // Add smaller value to result
       result.push(arr1[0] <= arr2[0] ? arr1.shift() : arr2.shift());
     }
     // Add the leftover array to result and return
     return result.concat(arr1.length ? arr1 : arr2);
   }
 
-  // If the list is of length 0 or 1 then it is already sorted
+  // Base case: if the list is of length 0 or 1 then it is already sorted
   if (arr.length < 2) {
     return arr;
   }
---
+++ quickSort.js
@@ -0,0 +1,29 @@
+/*
+Quick Sort:
+
+Quick sort is a divide and conquer sorting algorithm. It creates two empty arrays to hold elements less than the pivot value and elements greater than the pivot value, and then recurively sort the sub arrays. Javascript’s native .sort() function uses Quicksort.
+
+average: O(n log n)
+worst: O(n^2)
+*/
+
+
+function quicksort(arr) {
+  // Base case
+  if (arr.length === 0) {
+    return [];
+  }
+  var left = [], right = [], pivot = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    // Push each int in appropriate subarray according to pivot
+    if (arr[i] < pivot) {
+     left.push(arr[i]);
+    } else {
+     right.push(arr[i]);
+    }
+  }
+
+  // Quick sort each subarray
+  return quicksort(left).concat(pivot, quicksort(right));
+}
