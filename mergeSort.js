ed69881bec96416fc457ce143cafd12c70408787
Paige <paige.vogenthaler@gmail.com>
2017-03-02 18:28:36 -0800

    Make adjustments to optimize time complexity

--- mergeSort.js
+++ mergeSort.js
@@ -12,16 +12,21 @@ O(n log n)
 const mergeSort = function(arr) {
 
   const merge = function (arr1, arr2) {
-    let result = [];
+    let result = [], i = 0, j = 0;
     while (arr1.length && arr2.length) {
-      // Add smaller value to result
-      result.push(arr1[0] <= arr2[0] ? arr1.shift() : arr2.shift());
+      result.push(arr1[0] <= arr2[0] ? arr1[i++] : arr2[j++];
     }
-    // Add the leftover array to result and return
-    return result.concat(arr1.length ? arr1 : arr2);
+
+    while (i < arr1.length) {
+      result.push(arr[i++]);
+    }
+    while (j < arr2.length) {
+      result.push(arr2[j++]);
+    }
+
+    return result;
   }
 
-  // Base case: if the list is of length 0 or 1 then it is already sorted
   if (arr.length < 2) {
     return arr;
   }
@@ -30,6 +35,5 @@ const mergeSort = function(arr) {
   let left = arr.slice(0, mid);
   let right = arr.slice(mid, arr.length);
 
-  // Sort each sublist recursively by re-applying merge sort
   return merge(mergeSort(left), mergeSort(right));
 }
